e9b3e4d2262961a9ca0e67d4347b79a0ef212f97
Ashok Paudel <ashokpoudel2051@gmail.com>
2018-08-09 13:55:00 +0545

    database model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_08_111329) do
+ActiveRecord::Schema.define(version: 2018_08_09_071211) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "classrooms", force: :cascade do |t|
+    t.string "grade"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "subjects", force: :cascade do |t|
+    t.string "name"
+    t.bigint "classroom_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["classroom_id"], name: "index_subjects_on_classroom_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
