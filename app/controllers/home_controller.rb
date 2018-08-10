808e67623582a5e88cba8f21728206dd9738838a
bishosilwal <aayussilwal@gmail.com>
2018-08-10 10:41:44 +0545

    pry gem installed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!
   def index
+    if current_user.role == 'student'
+      render 'student_index.html.erb'
+    end
   end
 end
