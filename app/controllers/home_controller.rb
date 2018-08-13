fac6991b5c422ce34bb370c0bf7c2386500ddf20
bishosilwal <aayussilwal@gmail.com>
2018-08-13 17:34:55 +0545

    teacher home page created

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,9 @@ class HomeController < ApplicationController
     if current_user.role == 'student'
        @subjects = current_user.classroom.subjects.includes(:questions)
       render 'student_index.html.erb'
+    elsif current_user.role == 'teacher'
+      @subjects = current_user.subjects.includes(:questions)
+      render 'teacher_index.html.erb'
     end
   end
 end
