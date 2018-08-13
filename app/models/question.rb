9ec152635de48f7347ef2d1ad3459eafe5487f14
bishosilwal <aayussilwal@gmail.com>
2018-08-13 12:19:37 +0545

    answer given not given added

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,11 +1,11 @@
 class Question < ApplicationRecord
   belongs_to :subject
   belongs_to :user
-  before_create :default_answer
+  # before_create :default_answer
   default_scope {order(created_at: :desc)}
 
   private
-  def default_answer
-    self.answer = "This question has not been answer yet."
-  end
+  # def default_answer
+  #   self.answer = "This question has not been answer yet."
+  # end
 end
